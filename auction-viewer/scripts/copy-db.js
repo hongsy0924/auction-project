d5dbc0bcc305b1fd9c7fb748225ca90a9cc87b81
Soonyoung Hong <soonyoung@Hongui-MacBookAir.local>
2025-05-06 20:34:49 +0900

    change

--- auction-viewer/scripts/copy-db.js
+++ auction-viewer/scripts/copy-db.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-const src = path.resolve(__dirname, "../../auction-database/database/auction_list.db");
+const src = path.resolve(__dirname, "../../auction-database/database/auction_data.db");
 const destDir = path.resolve(__dirname, "../database");
-const dest = path.resolve(destDir, "auction_list.db");
+const dest = path.resolve(destDir, "auction_data.db");
 
 // 디렉토리 없으면 생성
 if (!fs.existsSync(destDir)) {
